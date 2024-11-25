64ffc0b402e6fb74df1a9d8d59e9d140ff7012d7
asqre <amitanand.asqre@gmail.com>
2024-11-25 14:58:55 +0530

    add content to dashboard

--- admin/src/pages/admin/Dashboard.jsx
+++ admin/src/pages/admin/Dashboard.jsx
@@ -1,15 +1,26 @@
 import Layout from "@/components/layout/Layout";
+import { useUser } from "@clerk/clerk-react";
 import React from "react";
 
 const Dashboard = () => {
+  const user = useUser();
   return (
     <Layout>
-      <div className="bg-white rounded-lg shadow p-6">
-        <h1 className="text-2xl font-bold text-gray-900 mb-4">
-          Welcome to Your Dashboard
-        </h1>
+      <div className="min-h-screen bg-purple-50 flex flex-col items-center justify-center">
+        <div className="bg-white shadow-lg rounded-xl p-10 w-full max-w-lg">
+          <h1 className="text-4xl font-extrabold text-purple-600 mb-6">
+            Dashboard
+          </h1>
+          <p className="text-lg mb-6 font-bold text-purple-800 capitalize">
+            Hello {user ? user.username : user.fisrtName}!
+          </p>
+          <p>
+            {" "}
+            Welcome to Status page App, where you can manage your User status
+            with ease.
+          </p>
+        </div>
       </div>
-      Dashboard
     </Layout>
   );
 };
