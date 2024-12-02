0e060f8614e304e52bf9f357af36bcfe2a6552ea
asqre <amitanand.asqre@gmail.com>
2024-12-03 01:44:25 +0530

    fixed the cors error

--- server/src/index.js
+++ server/src/index.js
@@ -27,7 +27,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.HOST_URL,
+    origin: [process.env.HOST_URL, process.env.CLIENT_LOCALHOST_URL],
+    // origin: process.env.HOST_URL,
     methods: ["GET", "POST"],
   },
 });
@@ -76,10 +77,14 @@ io.on("connection", (socket) => {
 // middlewares
 app.use(
   cors({
-    // origin: process.env.HOST_URL,
-    origin: "*",
+    origin: [
+      process.env.HOST_URL,
+      process.env.CLIENT_LOCALHOST_URL,
+      "http://3.89.62.143:8000",
+    ],
+    // origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    // allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
